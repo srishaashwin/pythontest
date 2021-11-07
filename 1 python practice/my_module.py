1307ac816da50497137e41c3a44e1bb8639d6867
Your Name <you@example.com>
2021-11-07 19:10:16 +0530

    gommiting the python files

---
+++ 1 python practice/my_module.py
@@ -0,0 +1,13 @@
+def odd_even(a):
+    if(a%2==0):
+        print("the numb is even")
+    else:
+        print("the numb is odd")
+
+        
+        
+def palan(b):
+    if(b==b[::-1]):
+        print("palandrome")
+    else:
+        print("not palandrome")
